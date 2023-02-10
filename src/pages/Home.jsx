4c211e5195bd21c0f03308ee7acf79a3dcbc47c1
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-02-09 17:52:52 -1200

    Added Render functional component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 import { Card, FormField, Loader } from '../components'
 
+const Render = ({ data, title }) => {
+    if(data?.length > 0) {
+        return data.map((post) => <Card key={post._id} {...post} />)
+    }
+}
+
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
 
+    const [searchText, setSearchText] = useState('');
+ 
 
   return (
     <section className='max-w-7x1 mx-auto'>
@@ -26,7 +34,13 @@ const Home = () => {
                 </div>
             ) : (
                 <>
-                
+                    {searchText && (
+                        <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                            Showing results for <span className='text-[#222328]'>{searchText}</span>
+                        </h2>
+                    )}
+
+                    <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'></div>
                 </>
             )}
         </div>
