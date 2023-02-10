8b7388a131dc4354a2ce5ab2992469030887e6b2
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-02-09 21:05:44 -1200

    Added properties for two different FormField component

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -14,9 +14,50 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  };
+
+  const handleChange = (e) => {
+
+  };
+
+  const handleSurpriseMe = () => { //to call our utility function to ensure we get new prompts
+
+  }
    
   return (
-    <section>
+    <section className='max-w-7xl mx-auto '>
+      <div>
+          <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+          <p className='mt-2 text-[#666e75] text-[16px] max-w-[1000px]'> Create imaginative and visually 
+          stunning images through DALL-E AI and share them with the community</p>
+      </div>
+
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            LabelName='Your name'
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField 
+            LabelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='a pencil and watercolor drawing of a bright city in the future with flying cars'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
 
     </section>
   )
