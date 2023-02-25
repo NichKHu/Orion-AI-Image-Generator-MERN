ad725197a4d256d40c2084b1b87b49430fed2b66
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-02-24 17:20:48 -1200

    Finalized handle functions for CreatePost

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -15,8 +15,30 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
 
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
   };
 
   const handleSubmit = () => {
